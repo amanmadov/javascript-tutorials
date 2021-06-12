2028db38dcf3c5125c8fae51c18e6d17ce31d936
amanmadov <amanmadov@gmail.com>
2021-06-12 21:53:05 +0300

    Update FreeCodeCamp Notes.js
    Finished ES6 section

--- FreeCodeCamp Notes.js
+++ FreeCodeCamp Notes.js
@@ -5,7 +5,9 @@
 
 */
 
-// Note: Skipped some of the basics in "Basic Javascript" section 
+
+// Basic Javascript
+
 
 //#region Var 
 
@@ -222,6 +224,10 @@
     rangeOfNumbers(1, 5) // [1, 2, 3, 4, 5]
 
 //#endregion
+
+
+// ES6
+
 
 //#region Differences Between the "var" and "let" keywords 
 
@@ -643,8 +649,446 @@
 
     console.log(greeting);
 
+    // The console will display the strings "Hello, my name is Zodiac Hasbro! and I am 56 years old."
 
 //#endregion
+
+//#region Write Concise Object Literal Declarations Using Object Property Shorthand 
+
+// ES6 adds some nice support for easily defining object literals.
+// Consider the following code:
+
+    const getMousePosition = (x, y) => ({
+        x: x,
+        y: y
+    });
+
+// getMousePosition is a simple function that returns an object containing two properties. 
+// ES6 provides the syntactic sugar to eliminate the redundancy of having to write x: x. 
+// You can simply write x once, and it will be converted tox: x (or something equivalent) under the hood. 
+// Here is the same function from above rewritten to use this new syntax:
+
+    const getMousePosition = (x, y) => ({ x, y });
+
+    const createPerson = (name, age, gender) => {
+        return {
+          name: name,
+          age: age,
+          gender: gender
+        };
+      };
+
+    // simplified
+    const createPerson = (name, age, gender) => {
+    return {name, age, gender };
+    };
+
+//#endregion
+
+//#region Write Concise Declarative Functions with ES6 
+
+// When defining functions within objects in ES5, we have to use the keyword function as follows:
+
+    const person = {
+        name: "Taylor",
+        sayHello: function() {
+            return `Hello! My name is ${this.name}.`;
+        }
+    };
+
+// With ES6, you can remove the function keyword and colon altogether when defining functions in objects. 
+// Here's an example of this syntax:
+
+    const person = {
+        name: "Taylor",
+        sayHello() {
+            return `Hello! My name is ${this.name}.`;
+        }
+    };
+
+//#endregion
+
+//#region Use class Syntax to Define a Constructor Function 
+
+// ES6 provides a new syntax to create objects, using the class keyword.
+
+// It should be noted that the class syntax is just syntax, and not a full-fledged class-based implementation 
+// of an object-oriented paradigm, unlike in languages such as Java, Python, Ruby, etc.
+
+// In ES5, we usually define a constructor function and use the new keyword to instantiate an object.
+
+    var SpaceShuttle = function(targetPlanet){
+        this.targetPlanet = targetPlanet;
+    }
+    var zeus = new SpaceShuttle('Jupiter');
+
+// The class syntax simply replaces the constructor function creation:
+
+    class SpaceShuttle {
+        constructor(targetPlanet) {
+            this.targetPlanet = targetPlanet;
+        }
+    }
+    const zeus = new SpaceShuttle('Jupiter');
+
+// It should be noted that the class keyword declares a new function, to which a constructor is added. 
+// This constructor is invoked when new is called to create a new object.
+
+// Note: UpperCamelCase should be used by convention for ES6 class names, as in SpaceShuttle used above.
+
+// The constructor method is a special method for creating and initializing an object created with a class. 
+
+//#endregion
+
+//#region Use getters and setters to Control Access to an Object 
+
+// You can obtain values from an object and set the value of a property within an object.
+// These are classically called getters and setters.
+// Getter functions are meant to simply return (get) the value of an object's private variable 
+// to the user without the user directly accessing the private variable.
+
+// Setter functions are meant to modify (set) the value of an object's private variable 
+// based on the value passed into the setter function. 
+// This change could involve calculations, or even overwriting the previous value completely.
+
+    class Book {
+        constructor(author) {
+            this._author = author;
+        }
+        // getter
+        get writer() {
+            return this._author;
+        }
+        // setter
+        set writer(updatedAuthor) {
+            this._author = updatedAuthor;
+        }
+    }
+
+    const novel = new Book('anonymous');
+    console.log(novel.writer);
+    novel.writer = 'newAuthor';
+    console.log(novel.writer);
+
+// The console would display the strings "anonymous" and "newAuthor".
+
+// Notice the syntax used to invoke the getter and setter. They do not even look like functions. 
+// Getters and setters are important because they hide internal implementation details.
+
+// Note: It is convention to precede the name of a private variable with an underscore (_). 
+// However, the practice itself does not make a variable private.
+
+// Coding challenge:
+// Use the class keyword to create a Thermostat class. The constructor accepts a Fahrenheit temperature.
+// In the class, create a getter to obtain the temperature in Celsius and a setter to set the temperature in Celsius.
+// Remember that C = 5/9 * (F - 32) and F = C * 9.0 / 5 + 32, where F is the value of temperature in Fahrenheit, 
+// and C is the value of the same temperature in Celsius.
+// Note: When you implement this, you will track the temperature inside the class in one scale, either Fahrenheit or Celsius.
+// This is the power of a getter and a setter. You are creating an API for another user, 
+// who can get the correct result regardless of which one you track.
+// In other words, you are abstracting implementation details from the user.
+
+    class Thermostat {
+        constructor(temp) {
+        this._temperature = 5/9 * (temp - 32);
+        }
+        // getter
+        get temperature() {
+        return this._temperature;
+        }
+        // setter
+        set temperature(updatedTemp) {
+        this._temperature = updatedTemp;
+        }
+    }
+  
+    const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+    let temp = thermos.temperature; // 24.44 in Celsius
+    thermos.temperature = 26;
+    temp = thermos.temperature; // 26 in Celsius
+
+
+//#endregion
+
+//#region Create a Module Script 
+
+// JavaScript started with a small role to play on an otherwise mostly HTML web. 
+// Today, it’s huge, and some websites are built almost entirely with JavaScript. 
+// In order to make JavaScript more modular, clean, and maintainable; ES6 introduced a way to easily share code among JavaScript files. 
+// This involves exporting parts of a file for use in one or more other files, and importing the parts you need, where you need them. 
+// In order to take advantage of this functionality, you need to create a script in your HTML document with a type of module. 
+// Here’s an example:
+
+<script type="module" src="filename.js"></script>
+
+// A script that uses this module type can now use the import and export features you will learn about in the upcoming challenges.
+
+//#endregion
+
+//#region Use export to Share a Code Block 
+
+// Imagine a file called math_functions.js that contains several functions related to mathematical operations. 
+// One of them is stored in a variable, add, that takes in two numbers and returns their sum. 
+// You want to use this function in several different JavaScript files.
+// In order to share it with these other files, you first need to export it.
+
+    export const add = (x, y) => {
+        return x + y;
+    }
+
+// The above is a common way to export a single function, but you can achieve the same thing like this:
+
+    const add = (x, y) => {
+        return x + y;
+    }
+
+    export { add };
+
+// When you export a variable or function, you can import it in another file and use it without having to rewrite the code. 
+// You can export multiple things by repeating the first example for each thing you want to export, 
+// or by placing them all in the export statement of the second example, like this:
+
+    export { add, subtract };
+
+//#endregion
+
+//#region Reuse JavaScript Code Using import 
+
+// import allows you to choose which parts of a file or module to load. 
+// In the previous lesson, the examples exported add from the math_functions.js file. 
+// Here's how you can import it to use in another file:
+
+    import { add } from './math_functions.js';
+
+// Here, import will find add in math_functions.js, import just that function for you to use, and ignore the rest. 
+// The ./ tells the import to look for the math_functions.js file in the same folder as the current file. 
+// The relative file path (./) and file extension (.js) are required when using import in this way.
+
+// You can import more than one item from the file by adding them in the import statement like this:
+
+    import { add, subtract } from './math_functions.js';
+
+//#endregion
+
+//#region Use * to Import Everything from a File 
+
+// Suppose you have a file and you wish to import all of its contents into the current file. 
+// This can be done with the import * as syntax. 
+// Here's an example where the contents of a file named math_functions.js are imported into a file in the same directory:
+
+    import * as myMathModule from "./math_functions.js";
+
+// The above import statement will create an object called myMathModule. 
+// This is just a variable name, you can name it anything. 
+// The object will contain all of the exports from math_functions.js in it, 
+// so you can access the functions like you would any other object property. 
+// Here's how you can use the add and subtract functions that were imported:
+
+    myMathModule.add(2,3);
+    myMathModule.subtract(5,3);
+
+//#endregion
+
+//#region Create an Export Fallback with export default 
+
+// In the export lesson, you learned about the syntax referred to as a named export. 
+// This allowed you to make multiple functions and variables available for use in other files.
+
+// There is another export syntax you need to know, known as export default. 
+// Usually you will use this syntax if only one value is being exported from a file. 
+// It is also used to create a fallback value for a file or module.
+
+// Below are examples using export default:
+
+    export default function add(x, y) {
+        return x + y;
+    }
+
+    export default function(x, y) {
+        return x + y;
+    }
+
+// The first is a named function, and the second is an anonymous function.
+
+// Since export default is used to declare a fallback value for a module or file, 
+// you can only have one value be a default export in each module or file.
+// Additionally, you cannot use export default with var, let, or const
+
+//#endregion
+
+//#region Import a Default Export 
+
+// In the last challenge, you learned about export default and its uses. 
+// To import a default export, you need to use a different import syntax. 
+// In the following example, add is the default export of the math_functions.js file. 
+// Here is how to import it:
+
+    import add from "./math_functions.js";
+
+// The syntax differs in one key place. The imported value, add, is not surrounded by curly braces ({}). 
+// add here is simply a variable name for whatever the default export of the math_functions.js file is. 
+// You can use any name here when importing a default.
+
+//#endregion
+
+//#region Create a JavaScript Promise 
+
+// A promise in JavaScript is exactly what it sounds like - you use it to make a promise to do something, usually asynchronously. 
+// When the task completes, you either fulfill your promise or fail to do so. 
+// Promise is a constructor function, so you need to use the new keyword to create one. 
+// It takes a function, as its argument, with two parameters - resolve and reject. 
+// These are methods used to determine the outcome of the promise. 
+// The syntax looks like this:
+
+    const myPromise = new Promise((resolve, reject) => {
+
+    });
+
+//#endregion
+
+//#region Complete a Promise with resolve and reject 
+
+// A promise has three states: pending, fulfilled, and rejected. 
+// The promise you created in the last challenge is forever stuck in the pending state because you did not add a way to complete the promise. 
+// The resolve and reject parameters given to the promise argument are used to do this. 
+// resolve is used when you want your promise to succeed, and reject is used when you want it to fail. 
+// These are methods that take an argument, as seen below.
+
+    const myPromise = new Promise((resolve, reject) => {
+        if(condition) {
+            resolve("Promise was fulfilled");
+        } else {
+            reject("Promise was rejected");
+        }
+    });
+
+// The example above uses strings for the argument of these functions, but it can really be anything. 
+// Often, it might be an object, that you would use data from, to put on your website or elsewhere.
+
+//#endregion
+
+//#region Handle a Fulfilled Promise with then 
+
+// Promises are most useful when you have a process that takes an unknown amount of time 
+// in your code (i.e. something asynchronous), often a server request. 
+// When you make a server request it takes some amount of time, 
+// and after it completes you usually want to do something with the response from the server. 
+// This can be achieved by using the then method. 
+// The then method is executed immediately after your promise is fulfilled with resolve. Here’s an example:
+
+    myPromise.then(result => {
+    
+    });
+
+// result comes from the argument given to the resolve method.
+
+    const makeServerRequest = new Promise((resolve, reject) => {
+        // responseFromServer is set to true to represent a successful response from a server
+        let responseFromServer = true;
+        
+        if(responseFromServer) {
+            resolve("We got the data");
+        } else {  
+            reject("Data not received");
+        }
+    });
+    
+    makeServerRequest.then(result => {
+        console.log(result);
+    });
+
+//#endregion
+
+//#region Handle a Rejected Promise with catch 
+
+// catch is the method used when your promise has been rejected. 
+// It is executed immediately after a promise's reject method is called. 
+// Here’s the syntax:
+
+    myPromise.catch(error => {
+    
+    });
+
+// error is the argument passed in to the reject method.
+
+    const makeServerRequest = new Promise((resolve, reject) => {
+        // responseFromServer is set to false to represent an unsuccessful response from a server
+        let responseFromServer = false;
+        
+        if(responseFromServer) {
+            resolve("We got the data");
+        } else {  
+            reject("Data not received");
+        }
+    });
+    
+    makeServerRequest.then(result => {
+        console.log(result);
+    });
+    
+    makeServerRequest.catch(error => {
+        console.log(error);
+    });
+
+//#endregion
+
+
+// Regular Expressions 
+
+
+//#region Using the Test Method 
+
+// Regular expressions are used in programming languages to match parts of strings. 
+// You create patterns to help you do that matching.
+
+// If you want to find the word the in the string The dog chased the cat, 
+// you could use the following regular expression: /the/. 
+// Notice that quote marks are not required within the regular expression.
+
+// JavaScript has multiple ways to use regexes. 
+// One way to test a regex is using the .test() method. 
+// The .test() method takes the regex, applies it to a string (which is placed inside the parentheses), 
+// and returns true or false if your pattern finds something or not.
+
+    let testStr = "freeCodeCamp";
+    let testRegex = /Code/;
+    testRegex.test(testStr);
+
+// The test method here returns true.
+
+//#endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
