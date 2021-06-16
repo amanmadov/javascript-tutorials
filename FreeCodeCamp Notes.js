e6abaaa8485474258082c1b828d6a9e76d46f1b6
amanmadov <amanmadov@gmail.com>
2021-06-16 10:25:23 +0300

    Update FreeCodeCamp Notes.js
    Finished "Basic Algorithm Scripting" section

--- FreeCodeCamp Notes.js
+++ FreeCodeCamp Notes.js
@@ -2000,6 +2000,7 @@
 
 // Basic Data Structures
 
+
 //#region Use an Array to Store a Collection of Data 
 
 // The below is an example of the simplest implementation of an array data structure. 
@@ -2703,6 +2704,668 @@
     console.log(addFriend(user, 'Pete'));
 
 //#endregion
+
+
+// Basic Algorithm Scripting
+
+
+//#region Convert Celsius to Fahrenheit 
+
+// The algorithm to convert from Celsius to Fahrenheit is the temperature in Celsius times 9/5, plus 32.
+
+// You are given a variable celsius representing a temperature in Celsius. 
+// Use the variable fahrenheit already defined and assign it the Fahrenheit 
+// temperature equivalent to the given Celsius temperature. 
+// Use the algorithm mentioned above to help convert the Celsius temperature to Fahrenheit.
+
+    function convertToF(celsius) {
+        let fahrenheit = celsius * 9/5 + 32;
+        return fahrenheit;
+    }
+  
+    convertToF(30);
+
+//#endregion
+
+//#region Reverse a String 
+
+// Reverse the provided string.
+// You may need to turn the string into an array before you can reverse it.
+// Your result must be a string.
+
+    // Solution 1:
+    function reverseString(str) {
+        for (var reversedStr = "", i = str.length - 1; i >= 0; i--) {
+            reversedStr += str[i];
+        }
+        return reversedStr;
+    }
+
+    // Solution 2:
+    function reverseString(str) {
+        return Array.from(str).reverse().join((''));
+    }
+
+    reverseString("hello");
+
+//#endregion
+
+//#region Factorialize a Number 
+
+// Return the factorial of the provided integer.
+// If the integer is represented with the letter n, 
+// a factorial is the product of all positive integers less than or equal to n.
+// Factorials are often represented with the shorthand notation n!
+// For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
+// Only integers greater than or equal to zero will be supplied to the function.
+
+    // Solution 1:
+    function factorialize(num) {
+        for (var product = 1; num > 0; num--) {
+            product *= num;
+        }
+        return product;
+    }
+
+    factorialize(5);
+    
+    // Solution 2:
+    function factorialize(num) {
+        let result = num < 2 ? 1 : num * factorialize(num - 1);
+        return result;
+    }
+    
+    // Solution 3:
+    function factorialize(num, factorial = 1) {
+        return num < 0 ? 1 : (
+            new Array(num)
+            .fill(undefined)
+            .reduce((product, val, index) => product * (index + 1), 1)
+        );
+    }
+
+//#endregion
+
+//#region Find the Longest Word in a String 
+
+// Return the length of the longest word in the provided sentence.
+// Your response should be a number.
+
+    // Solution 1:
+    function findLongestWordLength(str) {
+        let words = str.split(' ');
+        let maxLength = 0;
+
+        for (let i = 0; i < words.length; i++) {
+            if (words[i].length > maxLength) {
+                maxLength = words[i].length;
+            }
+        }
+
+        return maxLength;
+    }
+
+    // Solution 2:
+    function findLongestWordLength(s) {
+        return s.split(' ')
+                .reduce(function(longest, word) {
+                    return Math.max(longest, word.length)
+                }, 0);
+    }
+
+    // Solution 3:
+    function findLongestWordLength(str) {
+        return Math.max(...str.split(" ").map(word => word.length));
+    }
+
+    // Solution 4:
+    function findLongestWordLength(str) {
+        // split the string into individual words
+        const words = str.split(" ");
+      
+        // words only has 1 element left that is the longest element
+        if (words.length == 1) {
+            return words[0].length;
+        }
+      
+        // if words has multiple elements, remove the first element
+        // and recursively call the function
+        return Math.max(
+            words[0].length,
+            findLongestWordLength(words.slice(1).join(" "))
+        );
+      }
+
+    // Solution 5:
+    function findLongestWordLength(str) {
+        return str.split(' ').sort((a, b) => b.length - a.length)[0].length;
+    }
+
+    findLongestWordLength("The quick brown fox jumped over the lazy dog");
+
+
+//#endregion
+
+//#region Return Largest Numbers in Arrays 
+
+// Return an array consisting of the largest number from each provided sub-array. 
+// For simplicity, the provided array will contain exactly 4 sub-arrays.
+
+// Remember, you can iterate through an array with a simple for loop, 
+// and access each member with array syntax arr[i].
+
+    // Solution 1:
+    function largestOfFour(arr) {
+        let results = [];
+        for (let i = 0; i < arr.length; i++) {
+            let largestNumber = arr[i][0];
+            for (let j = 1; j < arr[i].length; j++) {
+                if (arr[i][j] > largestNumber) {
+                    largestNumber = arr[i][j];
+                }
+            }
+            results[i] = largestNumber;
+        }
+        return results;
+    }
+
+    // Solution 2:
+    function largestOfFour(arr) {
+        return arr.map(function(group) {
+            return group.reduce(function(prev, current) {
+                return current > prev ? current : prev;
+            });
+        });
+    }
+
+    // Solution 3:
+    function largestOfFour(arr) {
+        return arr.map(Function.apply.bind(Math.max, null));
+    }
+
+    // Solution 4:
+    function largestOfFour(arr, finalArr = []) {
+        return !arr.length ? finalArr : largestOfFour(arr.slice(1), finalArr.concat(Math.max(...arr[0])))
+    }
+
+    // Solution 5:
+    function largestOfFour(arr) {
+        let mArray = [];
+        for(let i = 0; i < arr.length; i++){
+            mArray.push(Math.max(...arr[i]));
+        }
+        return mArray;
+    }
+
+    largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+
+//#endregion
+
+//#region Confirm the Ending 
+
+// Check if a string (first argument, str) ends with the given target string (second argument, target).
+
+// This challenge can be solved with the .endsWith() method, which was introduced in ES2015. 
+// But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.
+
+    // Solution 1:
+    function confirmEnding(str, target) {
+        return str.slice(str.length - target.length) === target;
+    }
+    
+    confirmEnding("Bastian", "n");
+
+    // Solution 2:
+    function confirmEnding(str, target) {
+        let re = new RegExp(target + "$", "i");
+        return re.test(str);
+    }
+
+    // Solution 3:
+    function confirmEnding(str, target) {
+        return str.slice(-target.length) === target
+    }
+
+//#endregion
+
+//#region Repeat a String Repeat a String 
+
+// Repeat a given string str (first argument) for num times (second argument). 
+// Return an empty string if num is not a positive number. 
+// For the purpose of this challenge, do not use the built-in .repeat() method.
+
+    // Solution 1:
+    function repeatStringNumTimes(str, num) {
+        if(num <= 0) return "";
+
+        let combinedStr = str
+        for(let i = 1; i < num; i++){
+            combinedStr = combinedStr + str;
+        }
+        return combinedStr;
+    }
+
+    repeatStringNumTimes("abc", 3);
+
+    // Solution 2:
+    function repeatStringNumTimes(str, num) {
+        var accumulatedStr = "";
+      
+        while (num > 0) {
+            accumulatedStr += str;
+            num--;
+        }
+      
+        return accumulatedStr;
+    }
+
+    // Solution 3:
+    function repeatStringNumTimes(str, num) {
+        if (num < 1) {
+            return "";
+        } else {
+            return str + repeatStringNumTimes(str, num - 1);
+        }
+    }
+
+    // Solution 4:
+    function repeatStringNumTimes(str, num) {
+        return num > 0 ? str + repeatStringNumTimes(str, num - 1) : '';
+    }
+
+//#endregion
+
+//#region Truncate a String 
+
+// Truncate a string (first argument) if it is longer than the given maximum string length (second argument). 
+// Return the truncated string with a ... ending.
+
+    // Solution 1:
+    function truncateString(str, num) {
+        return str.length > num ? str.slice(0, num) + "..." : str;
+    }
+
+    truncateString("A-tisket a-tasket A green and yellow basket", 8);
+
+//#endregion
+
+//#region Finders Keepers 
+
+// Create a function that looks through an array arr and returns the first element in it that passes a 'truth test'. 
+// This means that given an element x, the 'truth test' is passed if func(x) is true. 
+// If no element passes the test, return undefined.
+
+    // Solution 1:
+    function findElement(arr, func) {
+        let num = 0;
+        for (let i = 0; i < arr.length; i++) {
+            num = arr[i];
+            if (func(num)) {
+                return num;
+            }
+        }
+        return undefined;
+    }
+  
+    findElement([1, 2, 3, 4], num => num % 2 === 0);
+
+    // Solution 2:
+    function findElement(arr, func) {
+        return arr.find(func);
+    }
+
+    // Solution 3:
+    function findElement(arr, func) {
+        return arr[arr.map(func).indexOf(true)];
+    }
+
+    // Solution 4:
+    function findElement(arr, func) {
+        return arr.length && !func(arr[0]) ? findElement(arr.slice(1), func) : arr[0];
+    }
+
+//#endregion
+
+//#region Boo who 
+
+// Check if a value is classified as a boolean primitive. Return true or false.
+// Boolean primitives are true and false.
+
+    // Solution 1: 
+    function booWho(bool) {
+        if(bool === true || bool === false) return true;
+        return false;
+    }
+
+    booWho(null);
+
+    // Solution 2:
+    function booWho(bool) {
+        return typeof bool === "boolean";
+    }
+
+//#endregion
+
+//#region Title Case a Sentence 
+
+// Return the provided string with the first letter of each word capitalized. 
+// Make sure the rest of the word is in lower case.
+
+// For the purpose of this exercise, you should also capitalize connecting words like the and of.
+
+    // Solution 1:
+    function titleCase(str) {
+        var convertToArray = str.toLowerCase().split(" ");
+        var result = convertToArray.map(function(val) {
+            return val.replace(val.charAt(0), val.charAt(0).toUpperCase());
+        });
+        return result.join(" ");
+    }
+
+    titleCase("I'm a little tea pot");
+
+
+    // Solution 2:
+    function titleCase(str) {
+        return str.toLowerCase().replace(/(^|\s)\S/g, L => L.toUpperCase());
+    }
+
+    // Regex explanation:
+        // Find all non-whitespace characters (\S)
+        // At the beginning of string (^)
+        // Or after any whitespace character (\s)
+        // The g modifier searches for other such word pattern in the whole string and replaces them.
+
+        // This solution works with national symbols and accented letters as illustrated by following examples
+        // international characters: ‘бабушка курит трубку’ // -> ‘Бабушка Курит Трубку’
+        // accented characters: ‘località àtilacol’ // -> ‘Località Àtilacol’
+
+        
+    // Solution 3:
+    String.prototype.replaceAt = function(index, character) {
+        return (this.substr(0, index) + character + this.substr(index + character.length));
+    };
+
+    function titleCase(str) {
+        var newTitle = str.split(" ");
+        var updatedTitle = [];
+        for (var st in newTitle) {
+            updatedTitle[st] = newTitle[st].toLowerCase().replaceAt(0, newTitle[st].charAt(0).toUpperCase());
+        }
+        return updatedTitle.join(" ");
+    }
+
+//#endregion
+
+//#region Slice and Splice 
+
+// You are given two arrays and an index.
+// Copy each element of the first array into the second array, in order.
+// Begin inserting elements at index n of the second array.
+// Return the resulting array. The input arrays should remain the same after the function runs.
+
+    // Solution 1:
+    function frankenSplice(arr1, arr2, n) {
+        let localArr = arr2.slice();
+        localArr.splice(n, 0, ...arr1);
+        return localArr;
+    }
+
+    frankenSplice([1, 2, 3], [4, 5, 6], 1);
+
+    // Solution 2:
+    function frankenSplice(arr1, arr2, n) {
+        let localArray = arr2.slice();
+        for (let i = 0; i < arr1.length; i++) {
+            localArray.splice(n, 0, arr1[i]);
+            n++;
+        }
+        return localArray;
+    }
+
+//#endregion
+
+//#region Falsy Bouncer 
+
+// Remove all falsy values from an array.
+// Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+// Hint: Try converting each value to a Boolean.
+
+    // Solution 1:
+    function bouncer(arr) {
+        let newArray = [];
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i]) newArray.push(arr[i]);
+        }
+        return newArray;
+    }
+
+    // Solution 2:
+    function bouncer(arr) {
+        return arr.filter(Boolean);
+    }
+
+//#endregion
+
+//#region Where do I Belong 
+
+// Return the lowest index at which a value (second argument) should be inserted into 
+// an array (first argument) once it has been sorted. 
+// The returned value should be a number.
+
+// For example, getIndexToIns([1,2,3,4], 1.5) should return 1 
+// because it is greater than 1 (index 0), but less than 2 (index 1).
+
+// Likewise, getIndexToIns([20,3,5], 19) should return 2 
+// because once the array has been sorted it will look like [3,5,20] 
+// and 19 is less than 20 (index 2) and greater than 5 (index 1).
+
+    // Solution 1:
+    function getIndexToIns(arr, num) {
+        arr.sort((a, b) => a - b);
+        let i = 0;
+        while(i < arr.length){
+            if(arr[i] >= num) return i;
+            i++;
+        }
+        return i;
+    }
+
+    getIndexToIns([40, 60], 50);
+
+    // Solution 2:
+    function getIndexToIns(arr, num) {
+        arr.sort((a, b) => a - b);
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] >= num)
+            return i;
+        }
+        return arr.length;
+    }
+
+    // Solution 3:
+    function getIndexToIns(arr, num) {
+        return arr.filter(val => num > val).length;
+    }
+
+    // Solution 4:
+    function getIndexToIns(arr, num) {
+        // sort and find right index
+        let index = arr
+          .sort((curr, next) => curr - next)
+          .findIndex(currNum => num <= currNum);
+        // Returns index or total length of arr
+        return index === -1 ? arr.length : index;
+    }
+      
+    getIndexToIns([40, 60], 500);
+
+    // Solution 5:
+    function getIndexToIns(arr, num) {
+        return arr
+                .concat(num)
+                .sort((a, b) => a - b)
+                .indexOf(num);
+    }
+
+    getIndexToIns([1, 3, 4], 2);
+
+//#endregion
+
+//#region Mutations 
+
+// Return true if the string in the first element of the array contains 
+// all of the letters of the string in the second element of the array.
+
+// For example, ["hello", "Hello"], should return true because all of the 
+// letters in the second string are present in the first, ignoring case.
+
+// The arguments ["hello", "hey"] should return false because the string hello does not contain a y.
+// Lastly, ["Alien", "line"], should return true because all of the letters in line are present in Alien.
+
+    // Solution 1:
+    function mutation(arr) {
+        let arr1 = arr[0].toLowerCase().split("");
+        let arr2 = arr[1].toLowerCase().split("");
+        for (let i = 0; i < arr2.length; i++) {
+            if(!arr1.includes(arr2[i])) return false;
+        }
+        return true;
+    }
+
+    mutation(["hello", "hey"]);
+
+    // Solution 2: Procedural
+    function mutation(arr) {
+        let test = arr[1].toLowerCase();
+        let target = arr[0].toLowerCase();
+        for (let i = 0; i < test.length; i++) {
+        if (target.indexOf(test[i]) < 0) return false;
+        }
+        return true;
+    }
+
+    // Solution 2: Declarative
+    function mutation(arr) {
+        return arr[1]
+            .toLowerCase()
+            .split("")
+            .every(function(letter) {
+                return arr[0].toLowerCase().indexOf(letter) != -1;
+            });
+    }
+
+    // Solution 3: Recursive
+    function mutation([ target, test ], i = 0) {
+        target = target.toLowerCase();
+        test = test.toLowerCase();
+        return i >= test.length
+            ? true
+            : !target.includes(test[i])
+            ? false
+            : mutation([ target, test ], i + 1);
+    }
+
+//#endregion
+
+//#region Chunky Monkey 
+
+// Write a function that splits an array (first argument) into groups the length of size (second argument) 
+// and returns them as a two-dimensional array.
+
+    // Solution 1:
+    function chunkArrayInGroups(arr, size) {
+        let temp = [];
+        let result = [];
+        for (let a = 0; a < arr.length; a++) {
+            if (a % size !== size - 1) temp.push(arr[a]);
+            else {
+                temp.push(arr[a]);
+                result.push(temp);
+                temp = [];
+            }
+        }
+        if (temp.length !== 0) result.push(temp);
+        return result;
+    }
+
+    // Solution 2:
+    function chunkArrayInGroups(arr, size) {
+        let newArr = [];
+        for (let i = 0; i < arr.length; i += size) {
+            newArr.push(arr.slice(i, i + size));
+        }
+        return newArr;
+    }
+
+    // Solution 3:
+    function chunkArrayInGroups(arr, size) {
+        let newArr = [];
+        let i = 0;
+        while (i < arr.length) {
+            newArr.push(arr.slice(i, i + size));
+            i += size;
+        }
+        return newArr;
+    }
+
+    chunkArrayInGroups(["a", "b", "c", "d"], 2);
+
+    // Solution 4:
+    function chunkArrayInGroups(arr, size) {
+        let newArr = [];
+        while (arr.length > 0) {
+            newArr.push(arr.splice(0, size));
+        }
+        return newArr;
+    }
+
+    // Solution 5:
+    function chunkArrayInGroups(arr, size) {
+        if (arr.length <= size) {
+            return [arr];
+        } else {
+            return [arr.slice(0, size)].concat(
+            chunkArrayInGroups(arr.slice(size), size)
+            );
+        }
+    }
+
+//#endregion
+
+
+// Object Oriented Programming
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
