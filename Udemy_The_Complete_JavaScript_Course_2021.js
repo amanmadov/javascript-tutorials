7af3f09c8cba113f19c0dd825caa07b4610ded0b
amanmadov <amanmadov@gmail.com>
2021-07-07 14:15:22 +0300

    Update Udemy_The_Complete_JavaScript_Course_2021.js
    completed "Destructuring Objects"

--- Udemy_The_Complete_JavaScript_Course_2021.js
+++ Udemy_The_Complete_JavaScript_Course_2021.js
@@ -1589,5 +1589,113 @@ let jsCourse = {
 
 //#endregion
 
+//#region Destructuring Objects 
+
+    const restaurant = {
+        name: 'Classico Italiano',
+        location: 'Via Angelo Tavanti 23, Firenze, Italy',
+        categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+        starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+        mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+        openingHours: {
+            thu: {
+                open: 12,
+                close: 22,
+            },
+            fri: {
+                open: 11,
+                close: 23,
+            },
+            sat: {
+                open: 0, // Open 24 hours
+                close: 24,
+            },
+        },
+
+        order(starterIndex, mainIndex) {
+            return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+        },
+    };
+
+    const {name, openingHours, categories} = restaurant;
+    console.log(name, openingHours, categories); // Classico Italiano {thu: {…}, fri: {…}, sat: {…}} (4) ["Italian", "Pizzeria", "Vegetarian", "Organic"]
+
+    // variables with different names
+    const {name: restaurantName, openingHours: hours, categories: tags} = restaurant;
+    console.log(restaurantName, hours, tags); // Classico Italiano {thu: {…}, fri: {…}, sat: {…}} (4) ["Italian", "Pizzeria", "Vegetarian", "Organic"]
+
+    // If property does not exist use default values
+    const{menu = [], starterMenu: starters = []} = restaurant;
+    console.log(menu, starters);
+
+    // Mutating variables
+    let a = 111;
+    let b = 999;
+    const obj = {a: 23, b: 7, c: 14};
+    // cant do const{a, b} = obj; because a and b are already declared 
+    // cant do let {a, b} = obj; because it creates new variables called a and b
+
+    ({a, b} = obj);     // this is the way to mutate a and b
+    console.log(a,b);   // 23 7
+
+    // Nested objects
+    const {fri} = openingHours;
+    console.log(fri); // {open: 11, close: 23}
+
+    const {fri:{open, close}} = openingHours;
+    console.log(open,close); // 11 23
+
+    const {fri:{open: o, close: c}} = openingHours;
+    console.log(o,c); // 11 23
+
+
+    const restaurant = {
+        name: 'Classico Italiano',
+        location: 'Via Angelo Tavanti 23, Firenze, Italy',
+        categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+        starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+        mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+        openingHours: {
+            thu: {
+                open: 12,
+                close: 22,
+            },
+            fri: {
+                open: 11,
+                close: 23,
+            },
+            sat: {
+                open: 0, // Open 24 hours
+                close: 24,
+            },
+        },
+
+        order(starterIndex, mainIndex) {
+            return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+        },
+
+        orderDelivery: function ({starterIndex = 1, mainIndex = 0, time = '20:00', address}) {
+            console.log('Order recieved. ' + this.starterMenu[starterIndex] + ' ' + this.mainMenu[mainIndex] + ' to ' + address + ' at ' + time);
+        },
+    };
+
+    restaurant.orderDelivery({
+        time: '22:30',
+        address: 'Via del Sole, 21',
+        mainIndex: 2,
+        starterIndex: 2,
+    });
+
+    restaurant.orderDelivery({
+        address: 'Via del Sole, 21',
+        starterIndex: 2,
+    });
+    
+
+//#endregion
+
+
 
 //#endregion
