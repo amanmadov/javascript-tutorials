cd544eab2b0eb8228fd40d76586da93164696b64
amanmadov <amanmadov@gmail.com>
2021-07-09 09:11:11 +0300

    Update Udemy_The_Complete_JavaScript_Course_2021.js
    completed "Rest Pattern and Parameters"

--- Udemy_The_Complete_JavaScript_Course_2021.js
+++ Udemy_The_Complete_JavaScript_Course_2021.js
@@ -1766,8 +1766,82 @@ let jsCourse = {
 
 //#endregion
 
+//#region Rest Pattern and Parameters
+
+    // 1. Destructuring
+
+    // The spread operator is located on the right side of the = sign like shown below
+    const arr = [1, 2, ...[3, 4]];
+
+    // The rest operator is located on the left side of the = sign 
+    const [a, b, ...others] = [1, 2, 3, 4, 5];
+    console.log(a, b, others); // 1 2 [3, 4, 5]
+
+    const restaurant = {
+        name: 'Classico Italiano',
+        location: 'Via Angelo Tavanti 23, Firenze, Italy',
+        categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+        starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+        mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+        orderPizza(mainIngredient, ...otherIngredients) {
+            console.log(mainIngredient);
+            console.log(otherIngredients);
+          },
+    };
+
+    const openingHours = {
+        [weekdays[3]]: {
+          open: 12,
+          close: 22,
+        },
+        [weekdays[4]]: {
+          open: 11,
+          close: 23,
+        },
+        [weekdays[5]]: {
+          open: 0, // Open 24 hours
+          close: 24,
+        },
+      };
 
 
+    const [pizza, ,risotto, ...otherFood] = [...restaurant.mainMenu, ...restaurant.starterMenu];
+    console.log(pizza,risotto,otherFood); // Pizza Risotto (4) ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"]
+
+    // important: the rest pattern always must be the last in the destructuring assignment
+
+    // objects
+    const {sat, ...weekdays} = restaurant.openingHours;
+    console.log(weekdays);
+
+    // 2. Functions
+
+    const add = function(...numbers) {
+        let sum = 0; 
+        for (let i = 0; i < numbers.length; i++) { 
+            sum += numbers[i]; 
+        } 
+        console. log(sum); 
+    };
+
+    add(2,3);
+    add(5,3,7,2);
+    add(8,2,5,3,2,1,4);
+
+    const x = [23, 5, 7];
+    add(...x);
+
+    restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+    restaurant.orderPizza('mushrooms');
+
+// So, the spread and rests syntax both look exactly the same but they work in opposite ways depending on where they are used.
+// So the spread operator is used where we would otherwise write values, separated by a comma.
+// On the other hand the rest pattern is basically used where we would otherwise write variable names separated by commas.
+// So, again the rest pattern can be used where we would write variable names, separated by commas and not values separated by commas.
+
+
+//#endregion
 
 
 
