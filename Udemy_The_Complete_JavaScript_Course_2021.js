e23b2c654d01b9e56dafca20f011a6f5d3a55c2f
amanmadov <amanmadov@gmail.com>
2021-07-14 22:13:48 +0300

    Update Udemy_The_Complete_JavaScript_Course_2021.js
    completed "Looping Objects: Object Keys, Values and Entries"

--- Udemy_The_Complete_JavaScript_Course_2021.js
+++ Udemy_The_Complete_JavaScript_Course_2021.js
@@ -2208,4 +2208,53 @@ let jsCourse = {
 
 //#endregion
 
+//#region Looping Objects: Object Keys, Values and Entries 
+
+    const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+    const openingHours = {
+        [weekdays[3]]: {
+            open: 12,
+            close: 22,
+        },
+        [weekdays[4]]: {
+            open: 11,
+            close: 23,
+        },
+        [weekdays[5]]: {
+            open: 0,
+            close: 24,
+        },
+    };
+
+    // Property NAMES
+    const properties = Object.keys(openingHours);
+    console.log(properties); // (3) ["thu", "fri", "sat"]
+
+    let openStr = `We are open on ${properties.length} days: `;
+    for (const day of properties) {
+        openStr += `${day}, `;
+    }
+    console.log(openStr); // We are open on 3 days: thu, fri, sat, 
+
+    // Property VALUES
+    const values = Object.values(openingHours);
+    console.log(values);
+
+    // Entire object
+    const entries = Object.entries(openingHours);
+    console.log(entries);
+
+    // [key, value]
+    for (const [day, { open, close }] of entries) {
+        console.log(`On ${day} we open at ${open} and close at ${close}`);
+    }
+
+//#endregion
+
+
+
+
+
+
 //#endregion
