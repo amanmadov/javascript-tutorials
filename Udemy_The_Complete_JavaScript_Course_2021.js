d212fc40016d00722ec91a89263ee933fd7abf5e
amanmadov <amanmadov@gmail.com>
2021-07-14 12:14:57 +0300

    Update Udemy_The_Complete_JavaScript_Course_2021.js
    completed "Footbal Game Coding Challenge 1"

--- Udemy_The_Complete_JavaScript_Course_2021.js
+++ Udemy_The_Complete_JavaScript_Course_2021.js
@@ -1961,4 +1961,62 @@ let jsCourse = {
 
 //#endregion
 
+//#region Footbal Game Coding Challenge 1
+
+/* 
+
+    We're building a football betting app (soccer for my American friends 😅)!
+    Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+    1. Create one player array for each team (variables 'players1' and 'players2')
+    2. The first player in any player array is the goalkeeper and the others are field players. 
+       For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+    3. Create an array 'allPlayers' containing all players of both teams (22 players)
+    4. During the game, Bayern Munich (team 1) used 3 substitute players. 
+       So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+    5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+    6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, 
+       along with the number of goals that were scored in total (number of player names passed in)
+    7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+    
+    TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+    GOOD LUCK 😀
+
+*/
+
+// Solutions:
+
+    // 1.
+    const [players1, players2] = game.players;
+    console.log(players1, players2);
+
+    // 2.
+    const [gk, ...fieldPlayers] = players1;
+    console.log(gk, fieldPlayers);
+
+    // 3.
+    const allPlayers = [...players1, ...players2];
+    console.log(allPlayers);
+
+    // 4.
+    const players1Final = [...players1, 'Thiago', 'Coutinho', 'Periscic'];
+
+    // 5.
+    const { odds: { team1, x: draw, team2 }, } = game;
+    console.log(team1, draw, team2);
+
+    // 6.
+    const printGoals = function (...players) {
+        console.log(players);
+        console.log(`${players.length} goals were scored`);
+    };
+    // printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+    // printGoals('Davies', 'Muller');
+    printGoals(...game.scored);
+
+    // 7.
+    team1 < team2 && console.log('Team 1 is more likely to win');
+    team1 > team2 && console.log('Team 2 is more likely to win');
+
+//#endregion
+
 //#endregion
