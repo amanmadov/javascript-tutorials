036e97616e553f11ac8c291bfe234e01ac5d1387
amanmadov <amanmadov@gmail.com>
2021-07-14 22:03:02 +0300

    Update Udemy_The_Complete_JavaScript_Course_2021.js
    completed "Optional Chaining"

--- Udemy_The_Complete_JavaScript_Course_2021.js
+++ Udemy_The_Complete_JavaScript_Course_2021.js
@@ -2121,5 +2121,91 @@ let jsCourse = {
 
 //#endregion
 
+//#region Optional Chaining 
+
+    const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+    const openingHours = {
+        [weekdays[3]]: {
+            open: 12,
+            close: 22,
+        },
+        [weekdays[4]]: {
+            open: 11,
+            close: 23,
+        },
+        [weekdays[5]]: {
+            open: 0,
+            close: 24,
+        },
+    };
+
+    const restaurant = {
+        name: 'Classico Italiano',
+        location: 'Via Angelo Tavanti 23, Firenze, Italy',
+        categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+        starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+        mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+        openingHours,
+
+        order(starterIndex, mainIndex) {
+            return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+        },
+
+        orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+            console.log(
+            `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+            );
+        },
+
+        orderPasta(ing1, ing2, ing3) {
+            console.log(
+            `Here is your declicious pasta with ${ing1}, ${ing2} and ${ing3}`
+            );
+        },
+
+        orderPizza(mainIngredient, ...otherIngredients) {
+            console.log(mainIngredient);
+            console.log(otherIngredients);
+        },
+    };
+
+    console.log(restaurant.openingHours.mon); // undefined
+    console.log(restaurant.openingHours.mon.open); // cannot read property 'open' of undefined. undefined.open
+
+    // check if mon property exists
+    if (restaurant.openingHours.mon)
+    console.log(restaurant.openingHours.mon.open);
+
+    // check if openingHours and mon property exists
+    if (restaurant.openingHours && restaurant.openingHours.mon)
+    console.log(restaurant.openingHours.mon.open);
+
+    // WITH optional chaining
+    console.log(restaurant.openingHours.mon?.open);     // undefined
+    console.log(restaurant.openingHours?.mon?.open);    // undefined
+
+    // important: does not throw error if property is null or undefined
+
+    // Example
+    const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+    for (const day of days) {
+        const open = restaurant.openingHours[day]?.open ?? 'closed';
+        console.log(`On ${day}, we open at ${open}`);
+    }
+
+    // Methods can also be checked using ?
+    console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+    console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+
+    // Arrays can also be checked using ?
+    const users = [{ name: 'Jonas', email: 'hello@jonas.io' }];
+    console.log(users[0]?.name ?? 'User array empty');
+
+    // same as above
+    if (users.length > 0) console.log(users[0].name);
+    else console.log('user array empty');
+
+//#endregion
 
 //#endregion
