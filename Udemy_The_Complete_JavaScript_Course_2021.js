408f91be78db7668d627ce099ccf40eb6fb0130c
amanmadov <amanmadov@gmail.com>
2021-07-14 18:13:28 +0300

    Update Udemy_The_Complete_JavaScript_Course_2021.js
    completed "Enhanced Object Literals"

--- Udemy_The_Complete_JavaScript_Course_2021.js
+++ Udemy_The_Complete_JavaScript_Course_2021.js
@@ -2056,8 +2056,70 @@ let jsCourse = {
 
 //#endregion
 
+//#region Enhanced Object Literals 
 
+    const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+    const openingHours = {
+        [weekdays[3]]: {
+            open: 12,
+            close: 22,
+        },
+        [weekdays[4]]: {
+            open: 11,
+            close: 23,
+        },
+        [weekdays[5]]: {
+            open: 0, // Open 24 hours
+            close: 24,
+        },
 
+        // old syntax
+        // fri: {
+        //     open: 0, // Open 24 hours
+        //     close: 24,
+        // },
+
+    };
+
+    const restaurant = {
+        name: 'Classico Italiano',
+        location: 'Via Angelo Tavanti 23, Firenze, Italy',
+        categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+        starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+        mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+        // ES6 enhanced object literals
+        openingHours,
+        // same as openingHours:openingHours,
+
+        order(starterIndex, mainIndex) {
+            return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+        },
+
+        // old syntax
+        // order: function(starterIndex, mainIndex) {
+        //     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+        // },
+
+        orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+            console.log(
+            `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+            );
+        },
+
+        orderPasta(ing1, ing2, ing3) {
+            console.log(
+            `Here is your declicious pasta with ${ing1}, ${ing2} and ${ing3}`
+            );
+        },
+
+        orderPizza(mainIngredient, ...otherIngredients) {
+            console.log(mainIngredient);
+            console.log(otherIngredients);
+        },
+    };
+
+//#endregion
 
 
 //#endregion
