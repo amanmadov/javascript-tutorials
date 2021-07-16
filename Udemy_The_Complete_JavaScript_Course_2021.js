5d6321605aafc60217c20de4d2b3c0d2f81b3418
amanmadov <amanmadov@gmail.com>
2021-07-16 21:41:07 +0300

    Update Udemy_The_Complete_JavaScript_Course_2021.js
    completed "Maps: Iteration"

--- Udemy_The_Complete_JavaScript_Course_2021.js
+++ Udemy_The_Complete_JavaScript_Course_2021.js
@@ -2453,6 +2453,78 @@ let jsCourse = {
 
 //#endregion
 
+//#region Maps: Iteration 
+
+    const question = new Map([
+        ['question', 'What is the best programming language in the world?'],
+        [1, 'C'],
+        [2, 'Java'],
+        [3, 'JavaScript'],
+        ['correct', 3],
+        [true, 'Correct 🎉'],
+        [false, 'Try again!'],
+    ]);
+
+    console.log(question);
+
+    /*
+
+    Map(7) {"question" => "What is the best programming language in the world?", 1 => "C", 2 => "Java", 3 => "JavaScript", "correct" => 3, …}
+    [[Entries]]
+    0: {"question" => "What is the best programming language in the world?"}
+    1: {1 => "C"}
+    2: {2 => "Java"}
+    3: {3 => "JavaScript"}
+    4: {"correct" => 3}
+    5: {true => "Correct 🎉"}
+    6: {false => "Try again!"}
+    
+    */
+
+    // Convert object to map
+
+    const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+    const openingHours = {
+        [weekdays[3]]: {
+            open: 12,
+            close: 22,
+        },
+        [weekdays[4]]: {
+            open: 11,
+            close: 23,
+        },
+        [weekdays[5]]: {
+            open: 0,
+            close: 24,
+        },
+    };
+
+    console.log(Object.entries(openingHours));
+    const hoursMap = new Map(Object.entries(openingHours));
+    console.log(hoursMap);
+
+    // Quiz app
+    console.log(question.get('question'));
+    for (const [key, value] of question) {
+        if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+    }
+
+    // const answer = Number(prompt('Your answer'));
+    const answer = 3;
+    console.log(answer);
+    console.log(question.get(question.get('correct') === answer));
+
+    // Convert map to array
+    console.log([...question]);
+
+    // console.log(question.entries());
+    console.log([...question.keys()]);
+    console.log([...question.values()]);
+
+//#endregion
+
+
 
 
 //#endregion
