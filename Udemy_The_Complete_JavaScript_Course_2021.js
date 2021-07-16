1691f16574cc84ec1c780ae44a105f039db6fc6e
amanmadov <amanmadov@gmail.com>
2021-07-16 22:19:11 +0300

    Update Udemy_The_Complete_JavaScript_Course_2021.js
    completed "Working With Strings - Part 1"

--- Udemy_The_Complete_JavaScript_Course_2021.js
+++ Udemy_The_Complete_JavaScript_Course_2021.js
@@ -2578,5 +2578,45 @@ let jsCourse = {
 
 //#endregion
 
+//#region Working With Strings - Part 1 
+
+    const airline = 'TAP Air Portugal';
+    const plane = 'A320';
+    console.log(plane[0]);  // A
+    console.log(plane[1]);  // 3
+    console.log(plane[2]);  // 2
+    console.log('B737'[0]); // B
+
+    console.log(airline.length);    // 16
+    console.log('B737'.length);     // 4
+
+    console.log(airline.indexOf('r'));  // 6
+    console.log(airline.lastIndexOf('r'));  // 10
+    console.log(airline.indexOf('Portugal'));   // 8
+    console.log(airline.indexOf('portugal'));   // -1
+
+    console.log(airline.slice(4));  // Air Portugal
+    console.log(airline.slice(4, 7));   // Air
+    console.log(airline.slice(0, airline.indexOf(' ')));    // TAP
+    console.log(airline.slice(airline.lastIndexOf(' ') + 1));   // Portugal
+    console.log(airline.slice(-2)); // al
+    console.log(airline.slice(1, -1)); // AP Air Portuga
+
+    // Checking if seat is in the middle. B and E are middle seats
+    const checkMiddleSeat = function (seat) {
+        const s = seat.slice(-1);
+        if (s === 'B' || s === 'E') console.log('You got the middle seat 😬');
+        else console.log('You got lucky 😎');
+    };
+
+    checkMiddleSeat('11B'); // You got the middle seat 😬
+    checkMiddleSeat('23C'); // You got lucky 😎
+    checkMiddleSeat('3E');  // You got the middle seat 😬
+
+    console.log(new String('jonas')); // String {"jonas"}
+    console.log(typeof new String('jonas')); // object
+    console.log(typeof new String('jonas').slice(1)); // string
+
+//#endregion
 
 //#endregion
