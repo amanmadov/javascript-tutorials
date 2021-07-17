f8098c5d9497720b18dd4c0e2f3da4ad88ddfca1
amanmadov <amanmadov@gmail.com>
2021-07-17 12:55:55 +0300

    Update Udemy_The_Complete_JavaScript_Course_2021.js
    completed "The call and apply Methods"

--- Udemy_The_Complete_JavaScript_Course_2021.js
+++ Udemy_The_Complete_JavaScript_Course_2021.js
@@ -2982,7 +2982,55 @@ let jsCourse = {
     
 //#endregion
 
+//#region The call and apply Methods 
 
+    const lufthansa = {
+        airline: 'Lufthansa',
+        iataCode: 'LH',
+        bookings: [],
+        // book: function() {}
+        book(flightNum, name) {
+            console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`);
+            this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+        },
+    };
+
+    lufthansa.book(239, 'Jonas Schmedtmann');   // Jonas Schmedtmann booked a seat on Lufthansa flight LH239
+    lufthansa.book(635, 'John Smith');          // John Smith booked a seat on Lufthansa flight LH635
+
+    const eurowings = {
+        airline: 'Eurowings',
+        iataCode: 'EW',
+        bookings: [],
+    };
+
+    const book = lufthansa.book;
+    book(23, 'Sarah Williams'); // Cannot read property 'airline' of undefined at book
+    // because in regular function calls this kwyword points to undefined 
+
+    // Call method
+    book.call(eurowings, 23, 'Sarah Williams');
+    console.log(eurowings);
+
+    book.call(lufthansa, 239, 'Mary Cooper');
+    console.log(lufthansa);
+
+    const swiss = {
+        airline: 'Swiss Air Lines',
+        iataCode: 'LX',
+        bookings: [],
+    };
+    book.call(swiss, 583, 'Mary Cooper');
+
+    // Apply method
+    const flightData = [583, 'George Cooper'];
+    book.apply(swiss, flightData);
+    console.log(swiss);
+
+    // same as above but most prefered way
+    book.call(swiss, ...flightData);
+
+//#endregion
 
 //#endregion
 
