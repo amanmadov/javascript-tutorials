b08ede28b08ba6ed6b4bcfa5a11fea5d1d89631d
amanmadov <amanmadov@gmail.com>
2021-07-19 14:16:22 +0300

    Update Udemy_The_Complete_JavaScript_Course_2021.js
    completed "Creating DOM Elements"

--- Udemy_The_Complete_JavaScript_Course_2021.js
+++ Udemy_The_Complete_JavaScript_Course_2021.js
@@ -3597,6 +3597,27 @@ let jsCourse = {
 
 //#endregion
 
+//#region Creating DOM Elements 
+
+    const displayMovements = function (movements) {
+        containerMovements.innerHTML = '';
+        
+        movements.forEach(function (mov, i) {
+            const type = mov > 0 ? 'deposit' : 'withdrawal';
+            const html = `
+                <div class="movements__row">
+                    <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+                    <div class="movements__value">${mov}€</div>
+                </div>
+            `;
+            containerMovements.insertAdjacentHTML('afterbegin', html);
+        });
+    };
+
+    displayMovements(account1.movements);
+
+//#endregion
+  
 //#endregion
 
 
