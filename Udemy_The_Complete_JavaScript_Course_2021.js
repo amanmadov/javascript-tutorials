b744a886d00847d00c418f687d523a94672c26f7
amanmadov <amanmadov@gmail.com>
2021-07-20 20:37:59 +0300

    Update Udemy_The_Complete_JavaScript_Course_2021.js
    completed "Coding Challenge #3"

--- Udemy_The_Complete_JavaScript_Course_2021.js
+++ Udemy_The_Complete_JavaScript_Course_2021.js
@@ -3880,6 +3880,34 @@ let jsCourse = {
 
 //#endregion
 
+//#region Coding Challenge #3 
+
+/*
+
+    Rewrite the 'calcAverageHumanAge' function from the previous challenge, but this time as an arrow function, and using chaining!
+    TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+    TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+    GOOD LUCK 😀
+
+*/
+
+// Solution:
+
+    const calcAverageHumanAge = ages =>
+    ages
+        .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+        .filter(age => age >= 18)
+        .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+    // adults.length
+
+    const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+    const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+
+    console.log(avg1, avg2);
+
+//#endregion
+
 
 //#endregion
 
