5c857353944cb98c0816c61d6c49e10723e007ab
amanmadov <amanmadov@gmail.com>
2021-07-24 18:24:31 +0300

    Update Udemy_The_Complete_JavaScript_Course_2021.js
    completed "The Remainder Operator"

--- Udemy_The_Complete_JavaScript_Course_2021.js
+++ Udemy_The_Complete_JavaScript_Course_2021.js
@@ -4749,6 +4749,36 @@ let jsCourse = {
 
 //#endregion
 
+//#region The Remainder Operator 
+
+    console.log(5 % 2); // 1
+    console.log(5 / 2); // 5 = 2 * 2 + 1
+    console.log(8 % 3); // 2
+    console.log(8 / 3); // 8 = 2 * 3 + 2
+    console.log(6 % 2); // 0
+    console.log(6 / 2); // 3
+    console.log(7 % 2); // 1
+    console.log(7 / 2); // 3.5
+
+    const isEven = n => n % 2 === 0;
+
+    console.log(isEven(8));     // true
+    console.log(isEven(23));    // false
+    console.log(isEven(514));   // true
+
+    labelBalance.addEventListener('click', function () {
+        [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+            // 0, 2, 4, 6
+            if (i % 2 === 0) row.style.backgroundColor = 'orangered';
+            // 0, 3, 6, 9
+            if (i % 3 === 0) row.style.backgroundColor = 'blue';
+        });
+    });
+
+//#endregion
+
+
+
 //#endregion
 
 
