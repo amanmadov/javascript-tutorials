a029c8e691b21c12aa7c5c9efc2b46ccca1d38a9
amanmadov <amanmadov@gmail.com>
2021-07-24 18:18:33 +0300

    Update Udemy_The_Complete_JavaScript_Course_2021.js
    completed "Math and Rounding"

--- Udemy_The_Complete_JavaScript_Course_2021.js
+++ Udemy_The_Complete_JavaScript_Course_2021.js
@@ -4708,6 +4708,46 @@ let jsCourse = {
 
 //#endregion
 
+//#region Math and Rounding 
+
+    console.log(Math.sqrt(25)); // 5
+    console.log(25 ** (1 / 2)); // 5
+    console.log(8 ** (1 / 3));  // 2
+    console.log(Math.max(5, 18, 23, 11, 2));        // 23
+    console.log(Math.max(5, 18, '23', 11, 2));      // 23
+    console.log(Math.max(5, 18, '23px', 11, 2));    // NaN
+    console.log(Math.min(5, 18, 23, 11, 2));        // 2
+    console.log(Math.PI * Number.parseFloat('10px') ** 2); // 314.1592653589793
+    console.log(Math.trunc(Math.random() * 6) + 1); // 4
+
+    const randomInt = (min, max) => Math.floor(Math.random() * (max - min) + 1) + min;
+
+    // 0...1 -> 0...(max - min) -> min...max
+    // console.log(randomInt(10, 20)); // 17    
+
+    // Math.trunc simply removes any decimal part. But we have also other ways like Math.round().
+
+    // Rounding integers
+    console.log(Math.round(23.3));      // 23
+    console.log(Math.round(23.9));      // 24
+    console.log(Math.ceil(23.3));       // 24
+    console.log(Math.ceil(23.9));       // 24
+    console.log(Math.floor(23.3));      // 23
+    console.log(Math.floor('23.9'));    // 23
+    console.log(Math.trunc(23.3));      // 23
+    console.log(Math.trunc(-23.3));     // -23
+    console.log(Math.floor(-23.3));     // -24
+
+    // Rounding decimals
+    // important: toFixed() method will always return a string not a number and that's important to keep in mind.
+
+    console.log((2.7).toFixed(0));      // 3
+    console.log((2.7).toFixed(3));      // 2.700 
+    console.log((2.345).toFixed(2));    // 2.35
+    // this will return number
+    console.log(+(2.345).toFixed(2));   // 2.35
+
+//#endregion
 
 //#endregion
 
