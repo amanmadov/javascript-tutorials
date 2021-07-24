aacdae38ac516696ab8a1fa90806754fbdb92aea
amanmadov <amanmadov@gmail.com>
2021-07-24 19:08:50 +0300

    Update Udemy_The_Complete_JavaScript_Course_2021.js
    completed "Operations With Dates"

--- Udemy_The_Complete_JavaScript_Course_2021.js
+++ Udemy_The_Complete_JavaScript_Course_2021.js
@@ -4894,6 +4894,20 @@ let jsCourse = {
 
 //#endregion
 
+//#region Operations With Dates 
+
+    const future = new Date(2037, 10, 19, 15, 23);
+    console.log(+future); // 2142246180000
+
+    const calcDaysPassed = (date1, date2) => Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+
+    const days1 = calcDaysPassed(new Date(2037, 3, 4), new Date(2037, 3, 14));
+    console.log(days1); // 10
+
+    // If you need extra calculations like including time changes due to daylight saving changes, then you should use a date library like moment.js.
+
+//#endregion
+
 //#endregion
 
 
